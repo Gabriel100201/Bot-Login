a1315ca76dd087efd9f4c2b31d85691d47c21de0
Gabriel Funes <gabrielfunes100202@gmail.com>
2024-03-19 10:31:50 -0300

    Agregando validacion de encabezados

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'DELETE, POST, GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  next();
+});
+
+
 const PORT = process.env.PORT || 3000
 
 // Auth
