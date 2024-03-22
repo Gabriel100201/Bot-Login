cd470ded720a301ce330fc781e2524404c5dc88f
Ubuntu <ubuntu@ip-172-31-17-173.ec2.internal>
2024-03-22 00:02:46 +0000

    Cambio cors config

--- index.js
+++ index.js
@@ -10,12 +10,10 @@ const { syncDocker } = require('./src/sync');
 const app = express();
 app.use(bodyParser.json())
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'DELETE, POST, GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-  next();
-});
+const corsOptions = {
+  origin: '*'
+}
+app.use(cors(corsOptions))
 
 
 const PORT = process.env.PORT || 3000
