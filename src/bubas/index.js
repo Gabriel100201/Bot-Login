e6fbb22289eff7372122a8b3a7122d2d6ea70004
Gabriel Funes <gabrielfunes100202@gmail.com>
2024-03-24 18:23:57 -0300

    Quitando bubas

--- src/bubas/index.js
+++
@@ -1,8 +0,0 @@
- const axios = require('axios');
- const setTotalCosts = ({newCost}) => {
-  axios.post("http://localhost:3000/updateCosts", {
-    costToAdd: newCost
-  })
- }
-
- module.exports = {setTotalCosts}
