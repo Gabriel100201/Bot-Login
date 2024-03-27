18acb565ee895b91f48cbe94a041c0271019b63c
Gabriel Funes <gabrielfunes100202@gmail.com>
2024-03-27 20:19:30 -0300

    Quitando usuario expuesto

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const { setNewUser } = require("./src/db");
-
-const fn = async () => {
-  try {
-    const userCreated = await setNewUser({ userName: "asdas", password: "sdasd", imageId: "2b590f7c476f6c429886339b3052628ffaa414175697a821fa46627171723418", company: "2312", rol: "23123" });
-  } catch (error) {
-    console.error('Error al crear un nuevo usuario:', error);
-  }
-}
-
-fn()
+// Tests
